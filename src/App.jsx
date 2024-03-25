564d6b4efe96d8df62a3c310485a40c5de284ea1
Brandon Davis <wowzerss90@yahoo.com>
2024-03-25 11:19:17 -0400

    Added error check for negative duration value

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10,
 })
 
+const inputIsValid = userInput.duration >= 1;
+
 const handleChange = (inputIdentifier, newValue)=> {
   setUserInput(prevUserInput => {
     return { ...prevUserInput,
@@ -25,7 +27,8 @@ const handleChange = (inputIdentifier, newValue)=> {
     <>
       <Header />
       <UserInput onChangeInput={handleChange} userInput={userInput} />
-      <Results userInput={userInput}/>
+      { !inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      { inputIsValid && <Results userInput={userInput}/>}
     </>
   )
 }
